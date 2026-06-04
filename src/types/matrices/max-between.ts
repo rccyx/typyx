688954bf8a88d77bde228abc88dad5c8166b304f
rccyx <oss@rccyx.com>
2026-06-04 18:36:55 +0100

    set type

--- src/types/matrices/max-between.ts
+++ src/types/matrices/max-between.ts
@@ -1,3 +1,25 @@
-import { Numeric } from "../primitives";
+import { Numeric } from '../primitives';
 
-export type MaxBetween<N extends Numeric, M extends Numeric>
+export type MinPositive<
+  N1 extends Numeric,
+  N2 extends Numeric,
+  L extends unknown[] = [],
+> = L['length'] extends N1 | N2
+  ? L['length'] extends N1
+    ? N1
+    : N2
+  : MinPositive<N1, N2, [-1, ...L]>;
+
+export type MaxPositive<
+  A extends Numeric,
+  B extends Numeric,
+  A1 extends Numeric = A,
+  B1 extends Numeric = B,
+  areAllNegative extends boolean = false,
+> = A extends MinPositive<A, B>
+  ? areAllNegative extends true
+    ? A1
+    : B1
+  : areAllNegative extends true
+    ? B1
+    : A1;

b8b7bfabbac7a7ad7cd5d61bfece30564c1ed56b
rccyx <oss@rccyx.com>
2026-08-02 16:56:07 +0100

    use extract

--- src/types/arrays/intersection.ts
+++ src/types/arrays/intersection.ts
@@ -1,8 +1,9 @@
+type ArrayValue<T> = T extends readonly unknown[] ? T[number] : T;
 
 /**
  * Calculates the intersection of the types within an array `Arr` of tuple types.
  * @returns
- * The types that repeat, if exists
+ * The types present in every tuple, if any.
  * @example
  * ```ts
  * ArrayIntersection<[[1, 0, 1], [0, 1, -1], [0, 0, 1]]>; // 0 | 1
@@ -10,9 +11,7 @@
  * ArrayIntersection<[[1, 0], [-1, -1], [-8, -9]]>; // never
  * ```
  */
-export type ArrayIntersection<Arr extends unknown[]> = Arr extends [
-  infer S,
-  ...infer E,
-]
-  ? (S extends unknown[] ? S[number] : S) & ArrayIntersection<E>
-  : unknown;
+export type ArrayIntersection<Arr extends readonly unknown[]> =
+  Arr extends readonly [infer First, ...infer Rest]
+    ? Extract<ArrayValue<First>, ArrayIntersection<Rest>>
+    : unknown;
